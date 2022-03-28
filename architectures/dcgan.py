6b0772c4b67c26f404eff1f977dab64e993680e2
goddamnparticle <yusufsalkpm@gmail.com>
2022-03-28 06:53:58 +0300

    Added manual optimization procedure.

--- architectures/dcgan.py
+++ architectures/dcgan.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 
@@ -66,3 +67,26 @@ class Discriminator(nn.Module):
         out = out.view(out.shape[0], -1)
         output = self.output_layer(out)
         return output 
+
+def weight_init(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm2d") != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- gan_trainer.py
+++ gan_trainer.py
@@ -7,14 +7,15 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 from pytorch_lightning import LightningDataModule, LightningModule, Trainer
-from torch.utils.data import DataLoader, random_split
-from architectures.dcgan import Generator, Discriminator
+from torch.utils.data import DataLoader
+from architectures.dcgan import Generator, Discriminator, weight_init
 from torchvision.datasets import ImageFolder
 
 PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
 AVAIL_GPUS = min(1, torch.cuda.device_count())
 BATCH_SIZE = 64 if AVAIL_GPUS else 64
 NUM_WORKERS = int(os.cpu_count() / 2)
+IMG_SIZE = 96 
 
 
 class GAN(LightningModule):
@@ -31,12 +32,11 @@ class GAN(LightningModule):
     ):
         super().__init__()
         self.save_hyperparameters()
+        self.automatic_optimization = False
         # networks
-        self.generator = Generator(
-            latent_dim=self.hparams.latent_dim, img_size=self.hparams.img_size
-        )
-        self.discriminator = Discriminator(img_size=self.hparams.img_size)
-        self.validation_z = torch.randn(8, self.hparams.latent_dim)
+        self.generator = Generator(img_size=self.hparams.img_size).apply(weight_init)
+        self.discriminator = Discriminator(img_size=self.hparams.img_size).apply(weight_init)
+        self.validation_z = torch.randn(9, self.hparams.latent_dim)
         self.example_input_array = torch.zeros(2, self.hparams.latent_dim)
 
     def forward(self, z):
@@ -45,47 +45,45 @@ class GAN(LightningModule):
     def adversarial_loss(self, y_hat, y):
         return F.binary_cross_entropy(y_hat, y)
 
-    def training_step(self, batch, batch_idx, optimizer_idx):
+    def training_step(self, batch, batch_idx):
         imgs, _ = batch
+
+        optimizer_g, optimizer_d = self.optimizers()
+        optimizer_g.zero_grad()
 
         # sample noise
         z = torch.randn(imgs.shape[0], self.hparams.latent_dim)
         z = z.type_as(imgs)
-
         # train generator
-        if optimizer_idx == 0:
-            # generate images
-            self.generated_imgs = self(z)
-            # log sampled images
-            sample_imgs = self.generated_imgs[:6]
-            grid = torchvision.utils.make_grid(sample_imgs)
-            self.logger.experiment.add_image("generated_images", grid, 0)
-            # ground truth result (ie: all fake)
-            # put on GPU because we created this tensor inside training_loop
-            valid = torch.ones(imgs.size(0), 1)
-            valid = valid.type_as(imgs)
-            # adversarial loss is binary cross-entropy
-            g_loss = self.adversarial_loss(self.discriminator(self(z)), valid)
-            tqdm_dict = {"g_loss": g_loss}
-            output = OrderedDict({"loss": g_loss, "progress_bar": tqdm_dict, "log": tqdm_dict})
-            return output
+        # generate images
+        self.generated_imgs = self(z)
+
+        # ground truth result (ie: all fake)
+        # put on GPU because we created this tensor inside training_loop
+        valid = torch.ones(imgs.size(0), 1)
+        valid = valid.type_as(imgs)
+        # adversarial loss is binary cross-entropy
+        g_loss = self.adversarial_loss(self.discriminator(self(z)), valid)
+        self.manual_backward(g_loss)
+        optimizer_g.step()
 
         # train discriminator
-        if optimizer_idx == 1:
-            # Measure discriminator's ability to classify real from generated samples
-            # how well can it label as real?
-            valid = torch.ones(imgs.size(0), 1)
-            valid = valid.type_as(imgs)
-            real_loss = self.adversarial_loss(self.discriminator(imgs), valid)
-            # how well can it label as fake?
-            fake = torch.zeros(imgs.size(0), 1)
-            fake = fake.type_as(imgs)
-            fake_loss = self.adversarial_loss(self.discriminator(self(z).detach()), fake)
-            # discriminator loss is the average of these
-            d_loss = (real_loss + fake_loss) / 2
-            tqdm_dict = {"d_loss": d_loss}
-            output = OrderedDict({"loss": d_loss, "progress_bar": tqdm_dict, "log": tqdm_dict})
-            return output
+        # Measure discriminator's ability to classify real from generated samples
+        # how well can it label as real?
+        optimizer_d.zero_grad()
+        valid = torch.ones(imgs.size(0), 1)
+        valid = valid.type_as(imgs)
+        real_loss = self.adversarial_loss(self.discriminator(imgs), valid)
+        # how well can it label as fake?
+        fake = torch.zeros(imgs.size(0), 1)
+        fake = fake.type_as(imgs)
+        fake_loss = self.adversarial_loss(self.discriminator(self(z).detach()), fake)
+        # discriminator loss is the average of these
+        d_loss = (real_loss + fake_loss) / 2
+        self.manual_backward(d_loss)
+        optimizer_d.step()
+
+        self.log_dict({"d_loss": d_loss, "g_loss": g_loss}, prog_bar=True)
 
     def configure_optimizers(self):
         lr = self.hparams.lr
@@ -99,7 +97,7 @@ class GAN(LightningModule):
         z = self.validation_z.type_as(self.generator.model[0].weight)
         # log sampled images
         sample_imgs = self(z)
-        grid = torchvision.utils.make_grid(sample_imgs)
+        grid = torchvision.utils.make_grid(sample_imgs, padding=2, nrow=3, normalize=True)
         self.logger.experiment.add_image("generated_images", grid, self.current_epoch)
 
 
@@ -109,6 +107,7 @@ class CustomDS(LightningDataModule):
         data_dir: str = PATH_DATASETS,
         batch_size: int = BATCH_SIZE,
         num_workers: int = NUM_WORKERS,
+        img_size: int = IMG_SIZE,
     ):
         super().__init__()
         self.data_dir_train = "data/customds/train_frames"
@@ -118,7 +117,7 @@ class CustomDS(LightningDataModule):
 
         self.transform = transforms.Compose(
             [
-                transforms.Resize(64),
+                transforms.Resize(img_size),
                 transforms.ToTensor(),
                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
             ]
@@ -127,7 +126,7 @@ class CustomDS(LightningDataModule):
         # self.dims is returned when you call dm.size()
         # Setting default dims here because we know them.
         # Could optionally be assigned dynamically in dm.setup()
-        self.dims = (3, 64, 64)
+        # self.dims = (3, 64, 64)
 
     def prepare_data(self):
         # download
@@ -137,8 +136,7 @@ class CustomDS(LightningDataModule):
     def setup(self, stage=None):
         # Assign train/val datasets for use in dataloaders
         if stage == "fit" or stage is None:
-            dataset_full = ImageFolder(self.data_dir_train, transform=self.transform)
-            self.dataset_train, self.dataset_val = random_split(dataset_full, [4000, 116])
+            self.dataset_train = ImageFolder(self.data_dir_train, transform=self.transform)
 
         # Assign test dataset for use in dataloader(s)
         if stage == "test" or stage is None:
@@ -153,6 +151,6 @@ class CustomDS(LightningDataModule):
 
 
 dm = CustomDS()
-model = GAN(*dm.size())
-trainer = Trainer(gpus=AVAIL_GPUS, max_epochs=20, progress_bar_refresh_rate=20)
+model = GAN(channels=3, img_size=IMG_SIZE)
+trainer = Trainer(gpus=AVAIL_GPUS, max_epochs=20, progress_bar_refresh_rate=1)
 trainer.fit(model, dm)
